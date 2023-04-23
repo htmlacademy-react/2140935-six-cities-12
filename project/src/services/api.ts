602ff73c9e7b890c349a9f829b622aa22647d9f3
totorostudio <107058400+totorostudio@users.noreply.github.com>
2023-04-24 00:37:11 +0300

    Update api.ts

--- project/src/services/api.ts
+++ project/src/services/api.ts
@@ -2,19 +2,13 @@ import axios, {AxiosInstance, AxiosRequestConfig, AxiosResponse, AxiosError} fro
 import {getToken} from './token';
 import {processErrorHandle} from './process-error-handle';
 
-const HTTP_STATUS = {
-  BAD_REQUEST: 400,
-  UNAUTHORIZED: 401,
-  NOT_FOUND: 404,
-};
+enum HttpStatus {
+  BAD_REQUEST = 400,
+  UNAUTHORIZED = 401,
+  NOT_FOUND = 404,
+}
 
-const StatusCodeMapping: Record<number, boolean> = {
-  [HTTP_STATUS.BAD_REQUEST]: true,
-  [HTTP_STATUS.UNAUTHORIZED]: true,
-  [HTTP_STATUS.NOT_FOUND]: true,
-};
-
-const shouldDisplayError = (response: AxiosResponse) => !!StatusCodeMapping[response.status];
+const isError = (response: AxiosResponse) => Object.values(HttpStatus).includes(response.status);
 
 const BACKEND_URL = 'https://12.react.pages.academy/six-cities';
 const REQUEST_TIMEOUT = 5000;
@@ -39,7 +33,7 @@ export const createAPI = (): AxiosInstance => {
   api.interceptors.response.use(
     (response) => response,
     (error: AxiosError<{error: string}>) => {
-      if (error.response && shouldDisplayError(error.response)) {
+      if (error.response && isError(error.response)) {
         processErrorHandle(error.response.data.error);
       }
 
